898205bb0fc0bd8273bd0501edb9db3a6fed44ef
SerKomla <elorm.dzameshie@gmail.com>
2023-07-25 14:13:53 -0700

    update to _getenv and gitignore files

--- _getenv.c
+++ _getenv.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 
-
 /**
  * _getenv - gets the value of the global variable
  * @n_gbvar: n_gbvar of the global variable
@@ -8,29 +7,32 @@
  */
 char *_getenv(const char *n_gbvar)
 {
-        int i, j;
-        char *value;
+	int i, j;
+	char *value;
 
-        if (!n_gbvar)
-                return (NULL);
-        for (i = 0; environ[i]; i++)
-        {
-                j = 0;
-                if (n_gbvar[j] == environ[i][j])
-                {
-                        while (n_gbvar[j])
-                        {
-                                if (n_gbvar[j] != environ[i][j])
-                                        break;
-
-                                j++;
-                        }
-                        if (n_gbvar[j] == '\0')
-                        {
-                                value = (environ[i] + j + 1);
-                                return (value);
-                        }
-                }
-        }
-        return (0);
+	if (!n_gbvar)
+		return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (n_gbvar[j] == environ[i][j])
+		{
+			j = 0;
+			if (n_gbvar[j] == environ[i][j])
+			{
+				while (n_gbvar[j])
+				{
+					if (n_gbvar[j] != environ[i][j])
+						break;
+					j++;
+				}
+				if (n_gbvar[j] == '\0')
+				{
+					value = (environ[i] + j + 1);
+					return (value);
+				}
+			}
+		}
+		return (0);
+	}
 }
