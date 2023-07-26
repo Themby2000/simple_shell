70860337ee5f73180e2ad228b44433ca9fa20da6
Themby2000 <sthembie@gmail.com>
2023-07-26 13:35:24 -0700

    ctrl

---
+++ ctrl.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * handle_signal - voids a signal and prints prompt.
+ * @signal: signal to be ignored.
+ * Return: nothing.
+ */
+
+void handle_signal(int signal)
+{
+	(void)signal;
+	write(STDOUT_FILENO, "\n$ ", 3);
+
+}
